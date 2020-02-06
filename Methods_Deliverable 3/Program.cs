6a03d93840338428c02c172f9644cc8bfb43f43c
zhenw <zhenw@DESKTOP-A960KRD>
2020-02-06 16:45:07 -0500

    Add project files.

---
+++ Methods_Deliverable 3/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+    Author: Zhenwei Li
+    Date: 02/06/2020
+    Comments: This C# Console application code demonstrates the use of
+              Methods after getting a string from users. 
+
+*/
+
+using System;
+
+namespace Methods_Deliverable_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            //Get input from user
+            Console.WriteLine("Please enter your name.");
+
+            // user try-catch to prevent unexpected user input
+            try
+            {
+                string input = Console.ReadLine();
+
+                string message = PrintMessage(input);
+
+                Console.WriteLine(message);
+            } // End of try
+            catch
+            {   
+                //display message to let user know what to enter.
+                Console.WriteLine("You must enter a string value. Please exit and try again");
+                Console.WriteLine("Press any key to exit the program and try again ...");
+                Console.ReadKey(true);
+            }
+            
+        }
+        private static string PrintMessage(string get_input)
+        {
+            string final_message = "Hello " + get_input + "!";
+            return final_message;
+
+        }
+    }
+}
